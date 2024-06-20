5ca6ba891779e0844e5d5058e0080a964095fcaf
Yee Mon <khinyeemonthant@abank.com.mm>
2024-06-20 16:12:17 +0630

    initial commit for fileupload

---
+++ BlazorFileupload/Data/CustomerModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorFileupload.Data
+{
+    public class CustomerModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public  string FileName { get; set; }
+
+    }
+}
